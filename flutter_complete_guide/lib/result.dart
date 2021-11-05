5ed9914f562b1ffeaf2932e3a9a0991546d70b4b
Refat Bin Mustaque <refat.mustaque@imp-w026.impulsebdltd.lan>
2021-11-05 12:38:04 +0600

    complete quiz app

--- flutter_complete_guide/lib/result.dart
+++ flutter_complete_guide/lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetQuiz;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetQuiz);
 
   String get ResultPhrase {
     String resultText = 'You did it.';
@@ -22,10 +23,19 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        ResultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            ResultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          // ignore: deprecated_member_use
+          FlatButton(
+              onPressed: resetQuiz,
+              textColor: Colors.blue,
+              child: Text('Restart Quiz!'))
+        ],
       ),
     );
   }
